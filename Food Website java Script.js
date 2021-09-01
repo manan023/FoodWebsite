623c90b06c389f8eb578e8e307818af743ab1c55
Cyber <mananarora023@gmail.com>
2021-09-01 22:28:17 +0530

    Initial Commit

---
+++ Food Website java Script.js
@@ -0,0 +1,13 @@
+// //  // function for food slider
+// $(document).ready(function(){
+//     $('.foode-slider').slick({
+//           slidesToshow:3,
+//     });
+     
+//     $('.nav-trigger').click(function(){
+//           $('.site-content-wrapper').toggleclass('scaled');
+//     })
+    
+//  });
+    
+
